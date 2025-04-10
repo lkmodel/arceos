290af417985fc81d1283fbe8a29cd8b0f932aafc
ysyx_22051213-Zhang Yuchi <inchinaxiaofeng@gmail.com>
2025-04-10 19:37:05 +0800

    [add] move math from musl to this

--- ulib/mocklibc_lib/c/math/ldexp.c
+++ ulib/mocklibc_lib/c/math/ldexp.c
@@ -1,7 +1,6 @@
-// NOTE: `Std C impl based on musl 1.2.5`
 #include <math.h>
 
 double ldexp(double x, int n)
 {
-    return scalbn(x, n);
+	return scalbn(x, n);
 }
--- ulib/mocklibc_lib/c/math/llrintf.c
+++ ulib/mocklibc_lib/c/math/llrintf.c
@@ -1,9 +1,8 @@
-// NOTE: `Std C impl based on musl 1.2.5`
 #include <math.h>
 
 /* uses LLONG_MAX > 2^24, see comments in lrint.c */
 
 long long llrintf(float x)
 {
-    return rintf(x);
+	return rintf(x);
 }
