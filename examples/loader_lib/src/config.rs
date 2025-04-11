fab00f08caa3ed034b0cd4a90f6c6ac93b9c683a
ysyx_22051213-Zhang Yuchi <inchinaxiaofeng@gmail.com>
2025-04-11 18:02:34 +0800

    [fix] fix the file bug, add test

--- examples/loader_lib/src/config.rs
+++ examples/loader_lib/src/config.rs
@@ -14,3 +14,9 @@ pub const MAX_LIB_SIZE: usize = 0x10_0000;
 pub const LIB_START: usize = 0xffff_ffc0_8010_0000;
 /// 用于存储全局信息
 pub const GLOBAL_SOTRE: usize = 0xffff_ffc0_801f_fff0;
+/// 支持目录最长长度
+pub const FILE_NAME_LENGTH: usize = 255usize;
+
+/// 可用内存区域
+pub const SPACE_BOTTOM: usize = 0xffff_ffc0_8000_0000;
+pub const SPACE_TOP: usize = 0xffff_ffc0_8800_0000;
